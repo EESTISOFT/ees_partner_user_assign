4656d5baeeb4ce687cde0f67cb229c7b4309fa6b
giulio milani <giulpiomilani@gmail.com>
2019-02-06 14:31:47 +0000

    Add files via upload

---
+++ ees_partner_user_assign/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'EESTISOFT partner user assign',
+    'version': '12.0.4.8',
+    'author': 'EESTISOFT, ''Giulio Milani',
+    'category': 'Productivity',
+    'website': 'https://eestisoft.com',
+    'sequence': 2,
+    'summary': 'Adds group ',
+    'description': """
+Adds massive Salesperson on partners
+	
+Made with love.
+    """,
+    'depends': ['base','contacts'],
+    'data': ['views/ees_partner_user_assign.xml','views/ir.model.access.csv'],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
