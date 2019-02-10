14f925ee120bd913ad6a09bf4f980111e0130c6c
giulio milani <giulpiomilani@gmail.com>
2019-02-10 18:52:48 +0100

    Update __manifest__.py

--- ees_partner_user_assign/__manifest__.py
+++ ees_partner_user_assign/__manifest__.py
@@ -11,7 +11,7 @@ Adds massive Salesperson on partners
 	
 Made with love.
     """,
-    'images':['thumb.png'],
+    'images':['thumbs.png'],
 	'depends': ['base','contacts'],
     'data': ['views/ees_partner_user_assign.xml','views/ir.model.access.csv'],
     'installable': True,
